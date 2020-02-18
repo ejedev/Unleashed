9d6a7e197663cbad588e56b419d466d3191a4663
Evan Edwards <39352103+ejedev@users.noreply.github.com>
2020-02-18 10:10:41 -0800

    Updated Python version.

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.7.6',
+    'Programming Language :: Python :: 3.7',
   ],
 )
