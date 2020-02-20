6e1dd444f84f42aaba303fe5f38ca867ec8007b0
Evan Edwards <39352103+ejedev@users.noreply.github.com>
2020-02-19 17:48:15 -0800

    Updated version and support for Python 3.8

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'Unleashed',
   packages = ['Unleashed'],
-  version = '0.1.3',
+  version = '0.2',
   license='MIT',
   description = 'A Python library to interact with the Unleashed API.',
   author = 'ejedev',
   author_email = 'ev3098@gmail.com',
   url = 'https://github.com/ejedev/Unleashed',
-  download_url = 'https://github.com/ejedev/Unleashed/archive/v0.1.3.tar.gz',
+  download_url = 'https://github.com/ejedev/Unleashed/archive/v0.2.tar.gz',
   keywords = ['Unleashed', 'API'],
   install_requires=[
     'wheel',
@@ -20,5 +20,6 @@ setup(
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
   ],
 )
