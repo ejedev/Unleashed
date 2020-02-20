67758edf000c551b6d978e66c833578eede335cb
Evan Edwards <39352103+ejedev@users.noreply.github.com>
2020-02-19 18:35:20 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'Unleashed',
   packages = ['Unleashed'],
-  version = '0.2',
+  version = '0.2.1',
   license='MIT',
   description = 'A Python library to interact with the Unleashed API.',
   author = 'ejedev',
   author_email = 'ev3098@gmail.com',
   url = 'https://github.com/ejedev/Unleashed',
-  download_url = 'https://github.com/ejedev/Unleashed/archive/v0.2.tar.gz',
+  download_url = 'https://github.com/ejedev/Unleashed/archive/v0.2.1.tar.gz',
   keywords = ['Unleashed', 'API'],
   install_requires=[
     'wheel',
@@ -19,6 +19,8 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
   ],
 )
